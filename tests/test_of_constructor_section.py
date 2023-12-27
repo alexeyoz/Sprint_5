3713cdc317931db0613a8f08001d59be875de4b2
alexeyoz <av.oz@yandex.ru>
2023-12-27 17:50:10 +0300

    Change locators

--- tests/test_of_constructor_section.py
+++ tests/test_of_constructor_section.py
@@ -1,3 +1,5 @@
+import time
+
 from locators import Locators
 
 
@@ -5,15 +7,15 @@ class TestConstructor:
     # 13. Проверка перехода к разделу "Соусы"
     def test_sauces_section(self, driver):
         driver.find_element(*Locators.SAUCES_BUTTON).click()
-        assert "current" in driver.find_element(*Locators.SAUCES_BUTTON).get_attribute("class")  # проверка перехода
+        assert driver.find_element(*Locators.SWITCHING_TO_SAUCES).is_displayed()  # проверка перехода "Соусы"
 
     # 14. Проверка перехода к разделу "Начинки"
     def test_toppings_section(self, driver):
         driver.find_element(*Locators.TOPPINGS_BUTTON).click()
-        assert "current" in driver.find_element(*Locators.TOPPINGS_BUTTON).get_attribute("class")  # проверка перехода
+        assert driver.find_element(*Locators.SWITCHING_TO_TOPPINGS).is_displayed()  # проверка перехода "Начинки"
 
     # 15. Проверка перехода к разделу "Булки"
     def test_bread_section(self, driver):
         driver.find_element(*Locators.SAUCES_BUTTON).click()
         driver.find_element(*Locators.BUNS_BUTTON).click()
-        assert "current" in driver.find_element(*Locators.BUNS_BUTTON).get_attribute("class")  # проверка перехода
+        assert driver.find_element(*Locators.SWITCHING_TO_BUNS).is_displayed()  # проверка перехода "Булки"
